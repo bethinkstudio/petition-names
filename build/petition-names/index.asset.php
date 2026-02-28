5a11c9cb0a90f65e248aaf7993d9b19e749f1122
George Stephanis <daljo628@gmail.com>
2026-02-28 09:51:44 -0500

    Include .petition-names-gravatar in styles
    Add .petition-names-gravatar alongside .avatar in the editor and front-end SCSS so gravatar elements share the same sizing and border-radius. Rebuilds updated the generated CSS/RTL files and bumped the build asset version (index.asset.php).

--- build/petition-names/index.asset.php
+++ build/petition-names/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '08de418aa53c9cd29ac0');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0ce53edf9e3123d19b44');
